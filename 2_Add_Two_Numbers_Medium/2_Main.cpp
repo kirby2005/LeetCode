47971244d8ce9cd824ae56c0e5afac3ddc123ae9
yuan-PC\yuan <a@a.a>
2019-05-01 23:01:51 +0800

    2_Add_Two_Numbers_Medium: 考虑不同长度

--- 2_Add_Two_Numbers_Medium/2_Main.cpp
+++ 2_Add_Two_Numbers_Medium/2_Main.cpp
@@ -10,10 +10,13 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
 	int carry = 0;
 	ListNode* head = new ListNode(0);
 	ListNode* currentNode = head;
+
 	while (l1 && l2)
 	{
 		ListNode* newNode = new ListNode(0);
 		currentNode->next = newNode;
+		currentNode = newNode;
+
 		int count = l1->val + l2->val + carry;
 		if (count >= 10)
 		{
@@ -28,10 +31,33 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
 
 		l1 = l1->next;
 		l2 = l2->next;
-		currentNode = newNode;
 	}
 
-	return head;
+	ListNode* rest = l1 ? l1 : l2;
+	while (rest)
+	{
+		ListNode* newNode = new ListNode(0);
+		currentNode->next = newNode;
+		currentNode = newNode;
+		int count = rest->val + carry;
+		if (count >= 10)
+		{
+			currentNode->val = count % 10;
+			carry = (int)(count / 10);
+		}
+		else
+		{
+			currentNode->val = count;
+			carry = 0;
+		}
+
+		rest = rest->next;
+	}
+
+	currentNode = head->next;
+	delete(head);
+	head = nullptr;
+	return currentNode;
 }
 
 int main(int argc, char* argv[])
@@ -44,8 +70,12 @@ int main(int argc, char* argv[])
 	ListNode* b1 = new ListNode(5);
 	ListNode* b2 = new ListNode(6);
 	ListNode* b3 = new ListNode(4);
+	ListNode* b4 = new ListNode(1);
+	ListNode* b5 = new ListNode(2);
 	b1->next = b2;
 	b2->next = b3;
+	b3->next = b4;
+	b4->next = b5;
 
 	auto result = addTwoNumbers(a1, b1);
 
