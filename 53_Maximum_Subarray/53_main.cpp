c2f83ea10b82ff0a5643a9fa94813c67a12c489b
yuan-PC\yuan <a@a.a>
2019-11-27 23:21:08 +0800

    53_Maximum_Subarray: AC DP  reduce space complexity to O(1)

--- 53_Maximum_Subarray/53_main.cpp
+++ 53_Maximum_Subarray/53_main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-static vector<int> nums{ -2,1,-3,4,-1,2,1,-5,4 };
-//static vector<int> nums{ 8,-19,5,-4,20 };
+//static vector<int> nums{ -2,1,-3,4,-1,2,1,-5,4 };
+static vector<int> nums{ 8,-19,5,-4,20 };
 
 int maxSubArray(vector<int>& nums)
 {
@@ -11,14 +11,12 @@ int maxSubArray(vector<int>& nums)
 		return 0;
 	}
 
-	vector<int> maxEndValue;
-	maxEndValue.push_back(nums[0]);
 	int currentMax = nums[0];
+	int lastMax = currentMax;
 	for (int i = 1; i < nums.size(); ++i)
 	{
-		int lastMax = maxEndValue[i - 1];
 		int max = lastMax > 0 ? lastMax + nums[i] : nums[i];
-		maxEndValue.push_back(max);
+		lastMax = max;
 		currentMax = currentMax >= max ? currentMax : max;
 	}
 	
@@ -29,5 +27,5 @@ int main(int argc, char* argv[])
 {
 	int result = maxSubArray(nums);
 
-	getchar();
+	//getchar();
 }
